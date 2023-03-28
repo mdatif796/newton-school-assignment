9ad9f315a321161d9f601145b01cb5b173bf6caf
mdatif796 <mdatif796@gmail.com>
2023-03-28 09:20:46 +0530

    wrote useEffect hook for demonstration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { useState, useEffect } from "react";
+
 function App() {
-  return <div className="App"></div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Users</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
